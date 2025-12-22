3f5593e154fa259778ab24fbc758d149833271a2
Barbara Silva <barbara.silva@zup.com.br>
2025-12-22 11:54:01 -0300

    Exercicio 02 finalizado

--- src/lista/exercicio02.java
+++ src/lista/exercicio02.java
@@ -1,6 +1,20 @@
 package lista;
 
+import java.util.Scanner;
+
 public class exercicio02 {
-    int a = 2;
-    int b = 3;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insira o valor 'a': ");
+        int a = scanner.nextInt();
+        System.out.print("Insira o valor 'b': ");
+        int b = scanner.nextInt();
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("Novo valor int a: " +a);
+        System.out.println("Novo valor int b:" +b);
+    }
 }
